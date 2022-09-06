10e6cc3cb022e89c777c60e0ea77b700a343d41a
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-09-06 15:42:09 +0530

    Ability to add multiple address book

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -6,95 +6,94 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-	static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
-	List<Contacts> contactsArrayList = new ArrayList<>();
+    List<Contacts> contactsArrayList = new ArrayList<>();
 
-	public void addContact() {
-		Contacts contacts = new Contacts();
+    public void addContact() {
+        Contacts contacts = new Contacts();
 
-		System.out.println("Enter First Name:");
-		String firstName = scanner.next();
-		contacts.setFirstName(firstName);
+        System.out.println("Enter First Name:");
+        String firstName = scanner.next();
+        contacts.setFirstName(firstName);
 
-		System.out.println("Enter Last Name:");
-		String lastName = scanner.next();
-		contacts.setLastName(lastName);
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.next();
+        contacts.setLastName(lastName);
 
-		System.out.println("Enter Email:");
-		String email = scanner.next();
-		contacts.setEmail(email);
+        System.out.println("Enter Email:");
+        String email = scanner.next();
+        contacts.setEmail(email);
 
-		System.out.println("Enter Phone Number:");
-		String phoneNumber = scanner.next();
-		contacts.setPhoneNumber(phoneNumber);
+        System.out.println("Enter Phone Number:");
+        String phoneNumber = scanner.next();
+        contacts.setPhoneNumber(phoneNumber);
 
-		System.out.println("Enter Zip Code:");
-		String zip = scanner.next();
-		contacts.setZip(zip);
+        System.out.println("Enter Zip Code:");
+        String zip = scanner.next();
+        contacts.setZip(zip);
 
-		System.out.println("Enter City:");
-		String city = scanner.next();
-		contacts.setCity(city);
+        System.out.println("Enter City:");
+        String city = scanner.next();
+        contacts.setCity(city);
 
-		System.out.println("Enter State:");
-		String state = scanner.next();
-		contacts.setState(state);
+        System.out.println("Enter State:");
+        String state = scanner.next();
+        contacts.setState(state);
 
-		System.out.println("Enter Address:");
-		String address = scanner.next();
-		contacts.setAddress(address);
+        System.out.println("Enter Address:");
+        String address = scanner.next();
+        contacts.setAddress(address);
 
-		contactsArrayList.add(contacts);
-	}
+        contactsArrayList.add(contacts);
+    }
+    public void editContact() {
+        System.out.println("Edit contact of First Name:");
+        String firstName = scanner.next();
 
-	public void editContact() {
-		System.out.println("Edit contact of First Name:");
-		String firstName = scanner.next();
+        boolean isContactThere = false;
 
-		boolean isContactThere = false;
+        for(Contacts contact:contactsArrayList){
 
-		for (Contacts contact : contactsArrayList) {
+            if(firstName.equals(contact.getFirstName())){
 
-			if (firstName.equals(contact.getFirstName())) {
+                isContactThere =true;
+                System.out.println("Contact Found! Edit contact details now :");
+                contact.editContact();
+                System.out.println("Contact edited successfully!");
+                break;
 
-				isContactThere = true;
-				System.out.println("Contact Found! Edit contact details now :");
-				contact.editContact();
-				System.out.println("Contact edited successfully!");
-				break;
+            }
 
-			}
+        }
 
-		}
+        if(!isContactThere){
+            System.out.println("No record of contact with First Name "+firstName+" in the address book.");
+        }
 
-		if (!isContactThere) {
-			System.out.println("No record of contact with First Name " + firstName + " in the address book.");
-		}
+    }
 
-	}
+    public void deleteContact() {
+        System.out.println("Delete contact of First Name:");
+        String firstName = scanner.next();
 
-	public void deleteContact() {
-		System.out.println("Delete contact of First Name:");
-		String firstName = scanner.next();
+        boolean isContactThere = false;
 
-		boolean isContactThere = false;
+        for(Contacts contact:contactsArrayList){
+            if(firstName.equals(contact.getFirstName())){
 
-		for (Contacts contact : contactsArrayList) {
-			if (firstName.equals(contact.getFirstName())) {
+                isContactThere =true;
+                contactsArrayList.remove(contact);
+                System.out.println("Contact deleted successfully!");
 
-				isContactThere = true;
-				contactsArrayList.remove(contact);
-				System.out.println("Contact deleted successfully!");
+                break;
+            }
 
-				break;
-			}
+        }
 
-		}
+        if(!isContactThere){
+            System.out.println("No record of contact with First Name "+firstName+" in the address book.");
+        }
 
-		if (!isContactThere) {
-			System.out.println("No record of contact with First Name " + firstName + " in the address book.");
-		}
-
-	}
+    }
 }
