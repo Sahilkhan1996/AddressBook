96dd90bfcce584edee765234f9d72cbf554d4a7f
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-09-06 14:59:47 +0530

    Checking to create contacts

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbooksystem;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+        Contacts c1=new Contacts();
+        System.out.println(c1);
     }
 }
